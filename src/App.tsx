afc5526f70bd5cd5c15943cc0f6cb51db63d7097
KauanCavazani <kauan.cavazani@gmail.com>
2023-10-07 16:22:10 -0300

    conteudo da página

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,16 @@ function App() {
   return (
     <>
       <div>Bem vindo!</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis mollitia architecto cumque 
+        quam placeat. Similique consequatur nam ducimus, debitis laudantium obcaecati temporibus sunt mollitia ratione, quidem facere eum officiis 
+        modi?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis facere rem necessitatibus quos, 
+        consequatur nulla quisquam, iusto eligendi sint quaerat velit optio incidunt vero nobis enim. Voluptates 
+        quod adipisci culpa!
+      </p>
     </>
   )
 }
